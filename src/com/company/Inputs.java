a1862c0cdfe0ca2c4996d2141b8cac0dac9f552d
rkuchenthal <koochenthal0@gmail.co,=m>
2021-11-08 22:37:50 -0800

    Setup input functionality from cmd line

---
+++ src/com/company/Inputs.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Inputs {
+
+    int id,time,priority;
+
+    public Inputs(int id, int time, int priority){
+        this.id = id;
+        this.time = time;
+        this. priority = priority;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,63 @@
 package com.company;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception{
+	    // initialize all variables and lists
+        ArrayList<Inputs> inputs = new ArrayList();
+        String line;
+        String[] inputLine;
+
+        //Output that shows how to format user input of task with id,time, and priority
+        System.out.print("Enter triples: process id, time in ms, and priority:\n" +
+                "For example:\n" +
+                "1 12 0\n" +
+                "3 9 1\n" +
+                "2 99 9\n" +
+                "process 1 needs 12 ms and has priority 0, very high,\n" +
+                "process 3 needs 9 ms and has priority 1.\n" +
+                "and so on ... Input below:\n");
+
+        InputStreamReader r = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(r);
+
+        while((line = br.readLine()) != null){
+            //TODO remove test int
+            int test = 0;
+            //validate all inputs are integers
+            try{
+                //turns "1 2 3" -> arr[0 ="1", 1 ="2", 2 ="3"]
+                inputLine = line.split(" ");
+
+                //TODO remove print lines
+                System.out.println("Input line "+test+inputLine[2]);
+                System.out.print("ID: "+inputLine[0]);
+                System.out.print(" time: "+inputLine[1]);
+                System.out.println(" priority: "+inputLine[2]);
+
+                Inputs newInput = new Inputs(Integer.parseInt(inputLine[0]),
+                                             Integer.parseInt(inputLine[1]),
+                                             Integer.parseInt(inputLine[2]));
+                //Now add the line of input to the inputs arraylist
+                inputs.add(newInput);
+
+
+            }catch (NumberFormatException e){
+                System.out.println(e);
+                System.out.println("Input not acceptable, Integers only.");
+            }
+            //TODO remove test
+            test ++;
+        }
+
+        //now call all 3 schedulers 1 after another and have them all print the results
+        // once their done and before the next one starts
+
+
+
+
     }
 }
